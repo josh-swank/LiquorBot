34fa655fa2d476353a152492f8135709a50938a7
Joshua Swank <joshua.swank.3521@gmail.com>
2018-04-26 18:39:24 -0500

    Add install, thanks, and listening app routes

--- app.py
+++ app.py
@@ -5,8 +5,22 @@ from flask import Flask
 pyBot = bot.Bot()
 slack = pyBot.client
 
-
 app = Flask(__name__)
+
+
+@app.route("/install", methods=["GET"])
+def pre_install():
+    pass
+
+
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    pass
+
+
+@app.route("/listening", methods=["GET", "POST"])
+def hears():
+    pass
 
 
 if __name__ == '__main__':
