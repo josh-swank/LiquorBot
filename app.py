d9afda6f3f96bf44bb9fe812d79ca4456064865b
Joshua Swank <joshua.swank.3521@gmail.com>
2018-04-28 12:30:08 -0500

    Add missing event response to /listening route

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 
 
 import bot
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template, request
+import json
 
 
 pyBot = bot.Bot()
@@ -25,7 +26,22 @@ def thanks():
 
 @app.route("/listening", methods=["GET", "POST"])
 def hears():
-    raise NotImplementedError()
+    slack_event = json.loads(request.data)
+
+    # Slack URL Verification
+    if 'challenge' in slack_event:
+        raise NotImplementedError()
+
+    # Slack Token Verification
+    if pyBot.verification != slack_event.get('token'):
+        raise NotImplementedError()
+    
+    # Process Incoming Events from Slack
+    if 'event' in slack_event:
+        raise NotImplementedError()
+
+    return make_response("No event in Slack request",
+            404, {'X-Slack-No-Retry': 1})
 
 
 if __name__ == '__main__':
