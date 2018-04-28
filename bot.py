d569bd4df6f257380ef35ddd84018d032516a10c
Joshua Swank <joshua.swank.3521@gmail.com>
2018-04-28 10:54:10 -0500

    Finish Bot class __init__

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+# TODO: Logging
+
+
+import os
+
 from slackclient import SlackClient
 
 
@@ -5,5 +10,12 @@ class Bot(object):
     def __init__(self):
         super(Bot, self).__init__()
         self.name = "liquorbot"
+        self.emoji = ":tumbler_glass:"
+
+        self.oauth = {'client_id': os.environ.get('CLIENT_ID'),
+                      'client_secret': os.environ.get('CLIENT_SECRET'),
+                      'scope': 'bot'}
+        self.verification = os.environ.get('VERIFICATION_TOKEN')
 
         self.client = SlackClient("")
+        self.messages = {}
